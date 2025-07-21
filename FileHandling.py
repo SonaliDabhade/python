04fbf11dff4156bbcdf3b72297e5024873600f00
SonaliDabhade <sonaligaikwad9813@gmail.com>
2025-07-21 07:44:04 +0000

    file-handling-21-jul-25

---
+++ FileHandling.py
@@ -0,0 +1,65 @@
+# Write data
+# file = open("data.txt","w")
+# file.write("New data ")
+
+# Append data
+# file = open("data.txt","a")
+# file.write("New data ")
+
+# Read data
+# file = open("data.txt","r")
+# print(file.read())
+
+#Binary data
+# file = open("arr.exe","rb")
+# print(file.read())
+
+# cLose()
+# file = open("arr.exe","rb")
+# print(file.read())
+# file.close()
+# print(file.read())
+
+# with open syntax 
+# with open("data.txt" , "r+") as file:
+#     print("Old Data : ",file.read())
+#     file.write("Added New data here\n")
+
+# file.write("Added New data here\n")
+
+
+
+# read using for loop
+# file = open("data.txt","r")
+
+# for i in file:
+#     print(i)
+
+
+
+# Create New FIle 
+# if(open("newData.txt","x")):
+#     print("File Created successfully")
+
+
+
+# # ftell()
+
+
+# data = open("newData.txt","r")
+
+# print(data.tell())
+# data.read()
+# print(data.tell())
+
+
+# seek()
+
+
+# data = open("newData.txt","r")
+
+# print(data.tell())
+# data.seek(3)
+# print(data.tell())
+# print(data.read())
+
