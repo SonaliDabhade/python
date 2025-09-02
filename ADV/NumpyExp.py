c8724f5ff74b36f6762a5bc2e4d9e2e247a9cacc
SonaliDabhade <sonaligaikwad9813@gmail.com>
2025-09-02 09:02:54 +0000

    pandas02/09/2025

---
+++ ADV/NumpyExp.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+
+list1 = [1,2,3,34]
+
+numpyList = np.array(list1)
+numpyList2 = np.array([10,20,30])
+arr = np.array((1, 2, 3, 4, 5))
+
+print(arr)
+
+print(list1,type(list1),len(list1))
+print(numpyList,type(numpyList),len(numpyList))
+print(numpyList2,type(numpyList2),len(numpyList2))
+
+print(list1[0])
+print(numpyList[0])
+# print(np.__version__)
